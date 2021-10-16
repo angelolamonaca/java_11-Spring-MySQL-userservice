fb14c342a239c30e9d39069acd33c224b799a761
Angelo <angelo.lamonaca@gmail.com>
2021-10-16 22:07:02 +0200

    :passport_control: Overriding Spring UserDetailsService

--- src/main/java/com/angelolamonaca/userservice/service/UserServiceImpl.java
+++ src/main/java/com/angelolamonaca/userservice/service/UserServiceImpl.java
@@ -6,9 +6,15 @@ import com.angelolamonaca.userservice.repo.RoleRepo;
 import com.angelolamonaca.userservice.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -16,10 +22,30 @@ import java.util.List;
  * @version 1.0
  * @since 16/10/2021
  */
-@Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database");
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getName());
