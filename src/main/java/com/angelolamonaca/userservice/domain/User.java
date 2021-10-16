2b1ef2033faeeac34fbd9d370383ead871f71ea4
Angelo <angelo.lamonaca@gmail.com>
2021-10-16 16:28:47 +0200

    :construction: Adding User Entity

---
+++ src/main/java/com/angelolamonaca/userservice/domain/User.java
@@ -0,0 +1,29 @@
+package com.angelolamonaca.userservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * @author Angelo Lamonaca (https://www.angelolamonaca.com/)
+ * @version 1.0
+ * @since 16/10/2021
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
