13a8a28e88290548f9c787617e8722ef9d770bfb
Angelo <angelo.lamonaca@gmail.com>
2021-10-16 17:04:33 +0200

    :construction: Adding User Controller

---
+++ src/main/java/com/angelolamonaca/userservice/api/UserController.java
@@ -0,0 +1,28 @@
+package com.angelolamonaca.userservice.api;
+
+import com.angelolamonaca.userservice.domain.User;
+import com.angelolamonaca.userservice.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author Angelo Lamonaca (https://www.angelolamonaca.com/)
+ * @version 1.0
+ * @since 16/10/2021
+ */
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>>getUser() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+}
